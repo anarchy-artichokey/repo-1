80a72cd666e6a1ca65b47f63d6eb7aa650470c6c
anarchy-artichokey <perez.arturo2005@outlook.com>
2022-01-01 18:11:16 -0600

    continuing

--- game.py
+++ game.py
@@ -1,12 +1,20 @@
 import os
 import platform
+import time
+import sys
 
 operation = platform.system()
 operversion = platform.release()
+
 if operation == 'Windows':
     cmd = 'mode 100,35'
     os.system(cmd)
 elif operation == 'Linux':
-    print("Youre on linux")
+    sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=100, cols=35))
+else:
+    print("You are on an unsupported operating system.\nMake sure the window is at least 100x35.")
+    time.sleep(10)
+
 print(operation + " " + operversion)
-print("GPL v3 anarchy-artichokey lonestar84")
+print("GPL-v3.0 anarchy-artichokey lonestar84")
+time.sleep(3)
